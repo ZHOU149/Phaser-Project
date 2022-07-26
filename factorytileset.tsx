b9328b81da9741cf6443e2a8cff91bbcd031d5de
colinwang05 <93939771+colinwang05@users.noreply.github.com>
2022-07-26 13:51:20 -0500

    Add files via upload

---
+++ factorytileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.0" name="factorytileset" tilewidth="32" tileheight="32" tilecount="416" columns="26">
+ <image source="factory_tileset.png" width="832" height="512"/>
+</tileset>
---
+++ scifitileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.0" name="scifitileset" tilewidth="32" tileheight="32" tilecount="851" columns="37">
+ <image source="tileset x1.png" width="1184" height="736"/>
+</tileset>
