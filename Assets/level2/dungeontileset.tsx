15dd665fa5f64e3b4a173c9651ad57ddaa5d00bc
Academy of Game Dev Camper <horns615@cs.utexas.edu>
2022-07-29 11:11:46 -0500

    did boss movement

--- Assets/level2/dungeontileset.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.0" name="dungeontileset" tilewidth="32" tileheight="32" tilecount="112" columns="4">
- <image source="Dungeon.png" width="128" height="896"/>
-</tileset>
--- Assets/level2/scifitileset.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.0" name="scifitileset" tilewidth="32" tileheight="32" tilecount="851" columns="37">
- <image source="scifitileset.png" width="1184" height="736"/>
-</tileset>
